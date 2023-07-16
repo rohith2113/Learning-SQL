e5b7067d7cad064e0402edbd0a35891e7653ad8a
rohith2113 <rohithkumar4476@gmail.com>
2023-07-16 17:26:21 +0530

    ALTER the table

--- All about SQL and it's categories/Data Defination Language Part - 2.sql
+++ All about SQL and it's categories/Data Defination Language Part - 2.sql
@@ -48,8 +48,30 @@ CREATE TABLE TEST_TBL2 (
     -- same as previous type but we are giving name for the constraints
 
 CREATE TABLE ALL_TEST3 (
-    TEST_COED VARCHAR2(10) CONSTRAINT TC_PK PRIMARY KEY,
+    TEST_CODE VARCHAR2(10) CONSTRAINT TC_PK PRIMARY KEY,
     TEST_DOMAIN VARCHAR2(20) CONSTRAINT TD_NN NOT NULL,
     TEST_TYPE CHAR(4) CONSTRAINT TT_NN NOT NULL
 );
 
+
+CREATE TABLE TEST_TBL3 (
+    TEST_ID INT CONSTRAINT TID_PK PRIMARY KEY,
+    TEST_NAME VARCHAR2(20) CONSTRAINT TNM_NN NOT NULL,
+    TEST_CODE VARCHAR2(10) CONSTRAINT TC_REF REFERENCES ALL_TEST3(TEST_CODE),
+    TEST_STATE CHAR(3),
+    TEST_SUBJ_CODE CHAR(4) CONSTRAINT TSCD_UNQ UNIQUE,
+    TEST_SUBJ_MARK INT CONSTRAINT TMRK_CHK CHECK ( TEST_SUBJ_MARK < 100 )
+);
+
+    -- naming a constraint is not mandatory but it is a good practice to name the constraint 
+    -- and it will be us to alter or disable or delete or modify the constraint
+    -- *NOT NULL constraints are under the type of "check" means they will check if it's null or not
+
+
+
+
+    -- Type-4
+    -- these create statements are used when you have to take a backup of a table, used mostly in real time
+    CREATE TABLE TEST4 AS SELECT * FROM ALL_TEST3;
+    -- selecting a existing table and recreating a bakcup table with the same data and structure
+    
